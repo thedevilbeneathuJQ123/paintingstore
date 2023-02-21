0606ee9daa1f53afa141d4993f411a7d5f0d1afe
thedevilbeneathu <jamalqussini123@gmail.com>
2023-02-22 00:32:07 +0200

    we put icons in the toolbar you have to change the name of the project to paintings store

---
+++ app/src/main/java/com/example/user_managment/paintingstore1.java
@@ -0,0 +1,82 @@
+package com.example.user_managment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toolbar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link paintingstore1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class paintingstore1 extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+       // Toolbar tb=getView().findViewById(R.id.toolbar);
+       // tb.setTitle("paintingstore");
+    }
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu,menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    public paintingstore1() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment paintingstore1.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static paintingstore1 newInstance(String param1, String param2) {
+        paintingstore1 fragment = new paintingstore1();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v= inflater.inflate(R.layout.fragment_paintingstore1, container, false);
+        setHasOptionsMenu(true);
+        return v;
+    }
+}
