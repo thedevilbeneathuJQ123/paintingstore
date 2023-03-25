da3c906bf63b8954a0e95fccb5259c7059d32e7e
thedevilbeneathu <jamalqussini123@gmail.com>
2023-03-25 16:56:56 +0300

    finished everything just need to fix the little problem that is why when i click on go to store it just quits.

---
+++ app/src/main/java/com/example/user_managment/CartActivity.java
@@ -0,0 +1,129 @@
+package com.example.user_managment;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.user_managment.adapter.MyCartAdapter;
+import com.example.user_managment.eventbus.MyUpdateCartEvent;
+import com.example.user_managment.listener.ICartLoadListener;
+import com.example.user_managment.model.CartModel;
+import com.example.user_managment.model.PaintingModel;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CartActivity extends AppCompatActivity implements ICartLoadListener {
+
+    @BindView(R.id.recycler_cart)
+    RecyclerView recyclerCart;
+    @BindView(R.id.Mainlayout)
+    RelativeLayout MainLayout;
+    @BindView(R.id.btnBack)
+    ImageView btnBack;
+    @BindView(R.id.txtTotal)
+    TextView txtTotal;
+    
+    ICartLoadListener cartLoadListener;
+
+    @Override
+    public void onStop() {
+        if (EventBus.getDefault().hasSubscriberForEvent(MyUpdateCartEvent.class))
+            EventBus.getDefault().removeStickyEvent(MyUpdateCartEvent.class);
+        EventBus.getDefault().unregister(this);
+        super.onStop();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN,sticky = true)
+    public void onUpdatecart(MyUpdateCartEvent event)
+    {
+        loadCartFromFirebase();
+    }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+        
+        Init();
+        loadCartFromFirebase();
+    }
+
+    private void loadCartFromFirebase() {
+        List<CartModel> cartModels = new ArrayList<>();
+        FirebaseDatabase.getInstance()
+                .getReference("Cart")
+                .child("UNIQUE_USER_ID")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()){
+                            for (DataSnapshot cartsnapshot:snapshot.getChildren())
+                            {
+                                CartModel cartModel = cartsnapshot.getValue(CartModel.class);
+                                cartModel.setKey(cartsnapshot.getKey());
+                                cartModels.add(cartModel);
+                            }
+                            cartLoadListener.onCartLoadSuccess(cartModels);
+                        }
+                        else
+                            cartLoadListener.onCartLoadFailed("cart empty");
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                       cartLoadListener.onCartLoadFailed(error.getMessage());
+                    }
+                });
+    }
+
+    private void Init() {
+        ButterKnife.bind(this);
+        
+        cartLoadListener = this;
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerCart.setLayoutManager(layoutManager);
+        recyclerCart.addItemDecoration(new DividerItemDecoration(this,layoutManager.getOrientation()));
+        
+        btnBack.setOnClickListener(view -> finish());
+    }
+
+    @Override
+    public void onCartLoadSuccess(List<CartModel> cartModelList) {
+        double sum = 0;
+        for (CartModel cartModel : cartModelList)
+        {
+            sum+=cartModel.getTotalprice();
+        }
+        txtTotal.setText(new StringBuilder("$").append(sum));
+        MyCartAdapter adapter = new MyCartAdapter(this,cartModelList);
+        recyclerCart.setAdapter(adapter);
+    }
+
+    @Override
+    public void onCartLoadFailed(String message) {
+        Snackbar.make(MainLayout,message,Snackbar.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/com/example/user_managment/adapter/MyCartAdapter.java
@@ -0,0 +1,144 @@
+package com.example.user_managment.adapter;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.user_managment.R;
+import com.example.user_managment.eventbus.MyUpdateCartEvent;
+import com.example.user_managment.model.CartModel;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.checkerframework.checker.units.qual.C;
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class MyCartAdapter extends RecyclerView.Adapter<MyCartAdapter.MyCartViewHolder> {
+    private Context context;
+    private List<CartModel> cartModelList;
+
+    public MyCartAdapter(Context context, List<CartModel> cartModelList) {
+        this.context = context;
+        this.cartModelList = cartModelList;
+    }
+
+    @NonNull
+    @Override
+    public MyCartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyCartViewHolder(LayoutInflater.from(context)
+                .inflate(R.layout.layout_cart_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyCartViewHolder holder, int position) {
+        Glide.with(context)
+                .load(cartModelList.get(position).getImage())
+                .into(holder.imageView);
+        holder.txtPrice.setText(new StringBuilder("$").append(cartModelList.get(position).getPrice()));
+        holder.txtName.setText(new StringBuilder().append(cartModelList.get(position).getName()));
+        holder.txtQuantity.setText(new StringBuilder().append(cartModelList.get(position).getQuantity()));
+        
+        //Event
+        holder.btnMinus.setOnClickListener(view -> {
+            minusCartItem(holder,cartModelList.get(position));
+        });
+        holder.btnPlus.setOnClickListener(view -> {
+            plusCartItem(holder,cartModelList.get(position));
+        });
+        holder.btnDelete.setOnClickListener(view -> {
+            AlertDialog dialog = new AlertDialog.Builder(context)
+                    .setTitle("Delte item?")
+                    .setMessage("Do you really want to delete item?")
+                    .setNegativeButton("CANCEL", (dialogInterface, which) -> dialogInterface.dismiss())
+                    .setPositiveButton("ok", (dialogInterface2, which) -> {
+                        
+                        //temp remove
+                        notifyItemRemoved(position);
+                        
+                        deleteFromFirebase(cartModelList.get(position));
+                        dialogInterface2.dismiss();
+                    }).create();
+            dialog.show();
+        });
+    }
+
+    private void deleteFromFirebase(CartModel cartModel) {
+        FirebaseDatabase.getInstance()
+                .getReference("Cart")
+                .child("UNIQUE_USER_ID")
+                .child(cartModel.getKey())
+                .removeValue()
+                .addOnSuccessListener(aVoid-> EventBus.getDefault().postSticky(new MyUpdateCartEvent()));
+    }
+
+    private void plusCartItem(MyCartViewHolder holder, CartModel cartModel) {
+        cartModel.setQuantity(cartModel.getQuantity()+1);
+        cartModel.setTotalprice(cartModel.getQuantity()*Float.parseFloat(cartModel.getPrice()));
+
+        holder.txtQuantity.setText(new StringBuilder().append(cartModel.getQuantity()));
+        updateFirebase(cartModel);
+    }
+
+    private void minusCartItem(MyCartViewHolder holder, CartModel cartModel) {
+        if (cartModel.getQuantity() > 1)
+        {
+            cartModel.setQuantity(cartModel.getQuantity()-1);
+            cartModel.setTotalprice(cartModel.getQuantity()*Float.parseFloat(cartModel.getPrice()));
+            
+            //update quantity
+            holder.txtQuantity.setText(new StringBuilder().append(cartModel.getQuantity()));
+            updateFirebase(cartModel);
+        }
+    }
+
+    private void updateFirebase(CartModel cartModel) {
+        FirebaseDatabase.getInstance()
+                .getReference("Cart")
+                .child("UNIQUE_USER_ID")
+                .child(cartModel.getKey())
+                .setValue(cartModel)
+                .addOnSuccessListener(aVoid-> EventBus.getDefault().postSticky(new MyUpdateCartEvent()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return cartModelList.size();
+    }
+
+    public class MyCartViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.btnMinus)
+        ImageView btnMinus;
+        @BindView(R.id.btnPlus)
+        ImageView btnPlus;
+        @BindView(R.id.btnDelete)
+        ImageView btnDelete;
+        @BindView(R.id.imageView)
+        ImageView imageView;
+        @BindView(R.id.txtName)
+        TextView txtName;
+        @BindView(R.id.txtprice)
+        TextView txtPrice;
+        @BindView(R.id.txtQuantity)
+        TextView txtQuantity;
+        
+        Unbinder unbinder ;
+        public MyCartViewHolder(@NonNull View itemView) {
+            super(itemView);
+            unbinder = ButterKnife.bind(this,itemView);
+        }
+    }
+}
